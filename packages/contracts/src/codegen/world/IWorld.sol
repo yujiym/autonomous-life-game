9168dfb8c605ceff4ad00137ca408c82bb7ecd31
yamapyblack <s25050730r@gmail.com>
2023-05-21 22:36:55 +0900

    add Clear function

--- packages/contracts/src/codegen/world/IWorld.sol
+++ packages/contracts/src/codegen/world/IWorld.sol
@@ -6,6 +6,7 @@ pragma solidity >=0.8.0;
 import { IBaseWorld } from "@latticexyz/world/src/interfaces/IBaseWorld.sol";
 
 import { IAddSystem } from "./IAddSystem.sol";
+import { IClearSystem } from "./IClearSystem.sol";
 import { IJoinSystem } from "./IJoinSystem.sol";
 import { IMapSystem } from "./IMapSystem.sol";
 
@@ -13,6 +14,6 @@ import { IMapSystem } from "./IMapSystem.sol";
  * The IWorld interface includes all systems dynamically added to the World
  * during the deploy process.
  */
-interface IWorld is IBaseWorld, IAddSystem, IJoinSystem, IMapSystem {
+interface IWorld is IBaseWorld, IAddSystem, IClearSystem, IJoinSystem, IMapSystem {
 
 }
